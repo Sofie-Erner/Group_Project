b28b527463d4b4c1908c44f6e24902bc83b311d3
Sofie Nordahl Erner <Sofie-Erner@users.noreply.github.com>
2018-03-14 16:08:07 +0000

    Everything up to date with project

--- program/script.cpp
+++ program/script.cpp
@@ -14,11 +14,42 @@
 using namespace std;
 
 int main(){
-  int nx = 21; int ny = 21; float dx = 0.4; float dy = 0.4; // initialising values for grid
-  char fileName[] = "conditions3.txt"; // file for getting boundary conditions
+  char fileName[] = "conditions.txt"; // file for getting boundary conditions
+  // Read from file
+  ifstream inFile;
+  inFile.open(fileName); // open file
+  if (!inFile){ cout << "Unable to open file \n"; exit(1);} // if the file can't be opened
+  
+  // initialising values for grid
+  // variables for getting numbers
+  float a, NX, NY, dx, dy;
+  string line;
+  int q=0;
+  
+  getline(inFile,line); // get line from file
+  stringstream ss(line);
+
+  while (ss >> a){
+    if (q==0){
+      NX=a;
+    }
+    else if (q==1){
+      dx=a;
+    }
+    else if (q==2){
+      NY=a;
+    }
+    else if (q==3){
+      dy=a;
+    }
+    q = q+1;
+  }
+  
+  float nx = (2*NX+1)/dx;
+  float ny = (2*NY+1)/dy;
   
   Grid grid(nx,ny,dx,dy); // initialising grid
-  grid = grid_input(nx,ny,dx,dy,fileName); // filling grid with info from file
+  grid = grid_input(fileName); // filling grid with info from file
 
   // print out grid
   for (int i=0; i<nx; i++){ // loop over rows and columns
